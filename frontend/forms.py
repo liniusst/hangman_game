edbcf81de78762bb183a9cddd26ad8da9f88e31b
Linas Stonkus <liniusst@gmail.com>
2023-08-28 15:52:48 +0300

    fix not used imports

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, BooleanField, StringField, PasswordField
+from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo
 
 
